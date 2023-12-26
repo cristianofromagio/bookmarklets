d306f076560abcc8bf3b4f4d0ae3bd4ca1e6fac3
Cristiano Fromagio <cristianofromagio@gmail.com>
2023-12-26 03:42:44 -0300

    Improve move-handler building block

--- src/building_blocks/shared/partials/move-handler.js
+++ src/building_blocks/shared/partials/move-handler.js
@@ -2,34 +2,50 @@
  * @requires /shared/partials/utils.js
  */
 
+try {
+  if (PARTIAL_UTILS);
+} catch (e) {
+  alert("Requires utils.js");
+}
+
 let offset = [0, 0];
 let moveTriggered = false;
 
 let moveHandler = $("#moveHandler");
+let mouseMoveListener;
 
 if (!moveHandler) {
   moveHandler = create('span');
-  fill(moveHandler, 'innerHTML', 'move');
-  $(`#${BLOCK_NAME} summary`).appendChild(moveHandler);
+  fill(moveHandler, 'textContent', 'move');
+  let summaryEl = $(`#${BLOCK_NAME} summary`);
+  if (summaryEl) {
+    summaryEl.appendChild(moveHandler);
+  } else {
+    $(`#${BLOCK_NAME}`).appendChild(moveHandler);
+  }
 }
 
 moveHandler.id = 'moveHandler';
 fill(moveHandler, 'style', `
-  float: right;
+  position: absolute;
+  top: .375em;
+  right: .375em;
   font-weight: bolder;
-  padding: .125rem .5rem;
-  margin-top: -.125rem;
-  background-color: rgba(0,0,0,.1);
+  padding: .125em .5em;
+  background-color: rgba(0,0,0,.2);
+  text-shadow: 1px 1px 1px rgba(0,0,0,.5);
   border-radius: 3px;
   cursor: move;
+  color: #fff;
+  font-size: 12px;
+  font-family: sans-serif;
+  box-sizing: border-box;
 `);
-// fill(moveHandler, 'style.float', 'right');
-// fill(moveHandler, 'style.fontWeigth', 'bolder');
-// fill(moveHandler, 'style.padding', '.125rem .5rem');
-// fill(moveHandler, 'style.marginTop', '-.125rem');
-// fill(moveHandler, 'style.backgroundColor', 'rgba(0,0,0,.1)');
-// fill(moveHandler, 'style.borderRadius', '3px');
-// fill(moveHandler, 'style.cursor', 'move');
+
+fill(moveHandler, 'onclick', (ev) => {
+  ev.preventDefault();
+  ev.stopImmediatePropagation();
+});
 
 fill(moveHandler, 'onmousedown', (ev) => {
   moveTriggered = true;
@@ -42,28 +58,38 @@ fill(moveHandler, 'onmousedown', (ev) => {
     left - ev.clientX,
     top - ev.clientY
   ];
+
+  document.addEventListener('mousemove', handleMouseMove);
 });
 
-fill(moveHandler, 'onmouseup', () => {
+fill(moveHandler, 'onmouseup', (ev) => {
   moveTriggered = false;
+  ev.preventDefault();
+
+  document.removeEventListener('mousemove', handleMouseMove);
 });
 
-// this is useful if moveHandler is not small,
-//  otherwise, it falls behind the positioning style update and breaks out of moving
-fill(moveHandler, 'onmouseleave', () => {
-  moveTriggered = false;
-});
+// THIS IS MADE USELESS ONCE WE LISTEN TO THE DOCUMENT 'onMouseMove'
+// // this is useful if moveHandler is not small,
+// //  otherwise, it falls behind the positioning style update and breaks out of moving
+// fill(moveHandler, 'onmouseleave', () => {
+//   console.log('mouseleave');
+//   if (!moveTriggered) {
+//     moveTriggered = false;
+//   }
+// });
 
-fill(moveHandler, 'onmousemove', (ev) => {
+const handleMouseMove = (ev) => {
   if (!moveTriggered) return;
   ev.preventDefault();
 
   let e = $(`#${BLOCK_NAME}`);
 
-  // set right to default value, needed for calculations
+  // set other values to default, needed for calculations
   // (otherwise element stretch from 0-right to the new position)
   e.style.right = 'auto';
+  e.style.bottom = 'auto';
 
   e.style.left = Number(ev.clientX + offset[0]) + 'px';
   e.style.top  = Number(ev.clientY + offset[1]) + 'px';
-});
+};
